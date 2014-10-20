22b6773ef5416893714b585ae629a24b33a7ffbd
Dario Miss <dariomiss@hotmail.com>
2014-10-20 17:32:54 -0200

    Fixed integer attribute. Added User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020185211) do
+ActiveRecord::Schema.define(version: 20141020193123) do
 
   create_table "sprints", force: true do |t|
     t.integer  "duration"
